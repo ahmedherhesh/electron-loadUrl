186cbfe0c9108970b80fcd8f7f065f93fd7a5486
ahmedherhesh <ahmedherhesh3@gmail.com>
2023-09-02 16:13:20 +0300

    some work

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const createMainWindow = () => {
     const mainWindow = new BrowserWindow({
         title: 'Created By Ahmed Harhash, Call Center : 01152958015',
         width: 1200,
-        minWidth:1100,
+        minWidth: 1100,
         height: 700,
         autoHideMenuBar: true,
     });
-    mainWindow.loadURL('http://pos.test')
+    mainWindow.loadURL('http://localhost')
 }
 
 app.whenReady().then(() => {
